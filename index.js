8e20795100bbbb787254e58771bcfd11cbea50a5
Olivia Desgrossillier <oliviadesgrossillier@gmail.com>
2023-05-03 21:37:34 -0700

    complete fetch lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        })
+    }
+    
+    return fetch("http://localhost:3000/users", configurationObject) 
+    .then(resp => resp.json())
+    .then(obj => document.body.innerHTML = obj.id)
+    .catch(error => document.body.innerHTML = error.message)
+}
